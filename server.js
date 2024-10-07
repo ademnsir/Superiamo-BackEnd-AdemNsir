291e3a980b2ccedb24d299dbdec988b46b66332a
nsir adem <nsir.adem@esprit.tn>
2024-10-08 00:54:58 +0100

    3 commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors'); 
 const connectDB = require("./config/db");
 const app = express();
-const port = 3001;
+const port = 5000;
 const AuthRoute = require("./routes/auth");
 
 connectDB();

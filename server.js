9e78375cb1fd88b72e202df8f9dc8b4d6905d44d
nsir adem <nsir.adem@esprit.tn>
2024-10-07 01:57:46 +0100

    Retirer les fichiers build de l'index Git

--- server.js
+++ server.js
@@ -33,7 +33,9 @@ app.use(
 // Importer les routes d'authentification
 const authRoutes = require("./routes/auth");
 app.use("/api/auth", authRoutes);
-
+app.get('/', (req, res) => {
+  res.send('Bonjour le serveur est en marche');
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

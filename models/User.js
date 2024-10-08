d3b3930517cf3688938c8a49cb494bb4e223794b
nsir adem <nsir.adem@esprit.tn>
2024-10-08 01:10:22 +0100

    3 commit

--- models/User.js
+++ models/User.js
@@ -3,36 +3,39 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   nom: {
     type: String,
+    required: true,
   },
   prenom: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
+    required: true,
+    unique: true,
   },
-  datenaissance: {
-    type: String,
+  googleId: {
+    type: String, // Champ pour stocker l'ID Google
+    unique: true,
   },
-  telephone: {
-    type: String,
+  githubId: {
+    type: String, // Champ pour stocker l'ID GitHub
+    unique: true,
+  },
+  dateNaissance: {
+    type: Date,
   },
   adresse: {
     type: String,
-  }
+  },
+  numeroTelephone: {
+    type: String,
+    match: /^[0-9]{8}$/, // Doit correspondre à un numéro de téléphone à 8 chiffres
+  },
+  password: {
+    type: String,
+    minlength: 6,
+  },
 });
 
-UserSchema.pre('save', async function (next) {
-  try {
-    if (!this.id) {
-      // Générez un identifiant unique si celui-ci n'est pas déjà défini
-      const count = await mongoose.model('user').countDocuments();
-      this.id = count + 1;
-    }
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
-
-const User = mongoose.model("user", UserSchema);
-module.exports = User;
+module.exports = mongoose.model("User", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,20 @@
-// controllers/auth.js
-
-
 const express = require("express");
 const router = express.Router();
-const AuthController =require ("../controllers/auth")
+const { registerUser, loginUser, updateProfile, getGoogleUser, getGithubUser } = require("../controllers/auth");
 
-router.route("/register").post(AuthController.register);
-router.route("/update").post(AuthController.updateUserByEmail);
-router.route("/getuser").get(AuthController.getUserByEmail);
+// Route pour créer un utilisateur
+router.post("/register", registerUser);
 
+// Route pour connecter un utilisateur
+router.post("/login", loginUser);
+
+// Route pour récupérer les informations d'un utilisateur via son email (Google)
+router.post("/google-user", getGoogleUser);
+
+// Route pour récupérer les informations d'un utilisateur via son nom et prénom (GitHub)
+router.post("/github-user", getGithubUser);
+
+// Route pour mettre à jour le profil utilisateur
+router.put("/update-profile", updateProfile);
 
 module.exports = router;
